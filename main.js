1d2b1d50068c47716972275b95fbd9bd219d76c2
NadineSchuster <samdee004@gmail.com>
2023-01-13 16:39:27 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,162 @@
+"use strict";
+
+let button = document.querySelector("#button");
+let card = document.querySelector("#exercise-card");
+let counter = document.querySelector("#counter");
+let userFeedback = document.querySelector("#user-feedback");
+let userAnswers = [];
+
+class Question {
+  constructor(id, question, type) {
+    this.id = id;
+    this.question = question;
+    this.type = type;
+  }
+
+  cleanUp(container) {
+    let child = container.firstElementChild;
+
+    if (child) {
+      while (child) {
+        var next = child.nextElementSibling;
+        container.removeChild(child);
+        child = next;
+      }
+    }
+  }
+}
+
+class QuestionCard extends Question {
+  constructor(id, question, answers, type, userSelection) {
+    super(id, question, type);
+    this.answers = answers;
+    this.userSelection = userSelection;
+  }
+
+  createQuizCard() {
+    let q = document.createElement("h3");
+    q.textContent = this.question;
+    card.appendChild(q);
+    // shuffle order of answers :)
+    this.answers = randomizeExercise(this.answers);
+    for (let i = 0; i < this.answers.length; i++) {
+      let element = document.createElement("p");
+      element.textContent = this.answers[i];
+      element.addEventListener("click", function () {
+        // registriere Antwort
+        let activeSelection = document.querySelector(
+          "#exercise-card p.selected"
+        );
+        if (activeSelection) {
+          activeSelection.classList.remove("selected");
+        }
+        element.classList.add("selected");
+      });
+      card.appendChild(element);
+    }
+  }
+
+  saveUserSelection() {
+    let activeSelection = document.querySelector("#exercise-card p.selected");
+    if (activeSelection) {
+      userAnswers.push(activeSelection.textContent);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  giveFeedback(feedback) {
+    switch (feedback) {
+      case "missing":
+        let p = document.createElement("p");
+        p.textContent = "Bitte eine Antwort auswählen";
+        p.classList.add("selection-missing");
+        userFeedback.appendChild(p);
+        break;
+
+      default:
+        break;
+    }
+  }
+}
+
+let c = new QuestionCard(
+  1,
+  "Was ist dein Lieblingstier?",
+  ["Katzen", "Hunde", "Fische", "Ich mag keine Tiere, ich esse Tiere"],
+  "Tiere"
+);
+
+let d = new QuestionCard(
+  2,
+  "Wo kannst du dich entspannen?",
+  ["Strand", "Wald", "Zuhause", "Bibliothek"],
+  "Orte"
+);
+
+let e = new QuestionCard(
+  3,
+  "Was ist deine Lieblingsfarbe?",
+  ["Rot", "Gelb", "Grün"],
+  "Farben"
+);
+
+let exercise = [];
+exercise.push(c, d, e);
+exercise = randomizeExercise(exercise);
+
+let loadQuestion = function () {
+  exercise[counter.textContent].cleanUp(card);
+  exercise[counter.textContent].createQuizCard();
+  counter.textContent++;
+};
+loadQuestion();
+
+button.addEventListener("click", function (e) {
+  let element = exercise[counter.textContent];
+
+  element.cleanUp(userFeedback);
+  let selectionExists = element.saveUserSelection();
+
+  if (selectionExists) {
+    element.cleanUp(card);
+    element.createQuizCard();
+    counter.textContent++;
+
+    if (counter.textContent >= exercise.length) {
+      counter.textContent = 0;
+      exercise = randomizeExercise(exercise);
+    }
+  } else {
+    element.giveFeedback("missing");
+  }
+});
+
+// if (counter.textContent < exercise.length || counter.textContent == 0) {
+//   console.log("add text");
+//   exercise[counter.textContent].createQuizCard();
+//   counter.textContent++;
+// } else {
+//   console.log("clean up");
+//   c.cleanUp();
+//   counter.textContent = 0;
+// }
+
+let sprüche = [
+  "Schön dass du da bist.",
+  "Schön dich zu sehen.",
+  "Ich freue mich, dass es dich gibt.",
+];
+
+function randomizeExercise(sprüche) {
+  for (let i = sprüche.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * i);
+    let k = sprüche[i];
+    sprüche[i] = sprüche[j];
+    sprüche[j] = k;
+  }
+
+  //   spruch.textContent = sprüche;
+  return sprüche;
+}
